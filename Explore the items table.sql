0f85f44d764dbd953e555ea452a59f50b5c03250
MagdalenaMW <mandzia01@gmail.com>
2025-03-11 19:48:09 +0100

    Update Explore the items table.sql

--- Explore the items table.sql
+++ Explore the items table.sql
@@ -1,14 +1,9 @@
--- 1. View the menu_items table.
-
-SELECT * 
-FROM menu_items;
-
--- 2. Find the number of items on the menu.
+-- 1. Find the number of items on the menu.
 
 SELECT COUNT(menu_item_id)
 FROM menu_items;
 
--- 3. What are the least and most expensive items on the menu?
+-- 2. What are the least and most expensive items on the menu?
 -- least expensive
 SELECT * 
 FROM menu_items
@@ -19,31 +14,26 @@ SELECT *
 FROM menu_items
 ORDER BY price DESC;
 
--- 4. How many Italian dishes are on the menu?
+-- 3. How many dishes are in each category?
 
-SELECT count(category) 
-FROM menu_items
-WHERE category = 'Italian';
-
--- 5. What are the least and most expensive Italian dishes on the menu?
--- least expensive
-SELECT * 
-FROM menu_items
-WHERE category = 'Italian'
-ORDER BY price;
-
--- most expensive
-SELECT * 
-FROM menu_items
-WHERE category = 'Italian'
-ORDER BY price DESC;
-
--- 6. How many dishes are in each category?
 SELECT category, COUNT(category) AS 'How many dishes'
 FROM menu_items
 GROUP BY category;
 
--- 7. What is the average dish price within each category?
+-- 4. What are the least and most expensive Italian dishes on the menu?
+-- least expensive
+SELECT * 
+FROM menu_items
+WHERE category = 'Italian'
+ORDER BY price;
+
+-- most expensive
+SELECT * 
+FROM menu_items
+WHERE category = 'Italian'
+ORDER BY price DESC;
+
+-- 5. What is the average dish price within each category?
 
 SELECT category, avg(price) AS 'Avg dish price'
 FROM menu_items
