a1447e5c11ce2c483486c5ef11822210d40e56cf
MagdalenaMW <mandzia01@gmail.com>
2025-03-11 20:03:46 +0100

    Update Explore the orders table.sql

--- Explore the orders table.sql
+++ Explore the orders table.sql
@@ -1,31 +1,26 @@
--- 1. View the order_details table.
-
-SELECT *
-FROM order_details;
-
--- 2. What is the date range of the table?
+-- 1. What is the date range of the table?
 
 SELECT MIN(order_date), MAX(order_date)
 FROM order_details;
 
--- 3. How many orders were made within this date range?
+-- 2. How many orders were made within this date range?
 
 SELECT COUNT(distinct order_id)
 FROM order_details;
 
--- 4. How many items were ordered within this date range?
+-- 3. How many items were ordered within this date range?
 
 SELECT COUNT(order_id)
 FROM order_details;
 
--- 5. Witch orders had the most number of items?
+-- 4. Witch orders had the most number of items?
 
 SELECT order_id, COUNT(item_id) AS 'num_items'
 FROM order_details
 GROUP BY order_id
 ORDER BY num_items DESC;
 
--- 6. How many orders had more than 12 items?
+-- 5. How many orders had more than 12 items?
 
 SELECT COUNT(*)
 FROM
